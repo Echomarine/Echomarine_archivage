9ae66d036e9be3d42255a648305e06ceeba4ea56
TracyGua <90851849+TracyGua@users.noreply.github.com>
2023-03-24 12:13:44 -0400

    commit essai

---
+++ Echo-interactivite/Echo_connexion/script/main.js
@@ -0,0 +1,19 @@
+//var btnCercle = document.getElementsByClassName('button');
+//btnCercle = i= [0];
+//var btnCercle = document.getElementsByClassName('#button');
+
+//const btnCercle = document.querySelector('.button');
+
+//btnCercle = i=[0];
+const son = new Audio('/media/Son/horse (1).mp3');
+
+function joueSon(){
+    son.play();
+}
+
+//console.log(btnCercle);
+
+//btnCercle.addEventListener("click", function(){
+   // son.play();
+ //onSendClick;
+//});
